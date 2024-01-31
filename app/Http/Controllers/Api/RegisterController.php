18597ecc3723a9ce0e26354a9f665dc7381e8043
Alan Novianto <77398526+Mayuzumi05@users.noreply.github.com>
2024-01-31 17:44:19 +0700

    access token

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -14,25 +14,38 @@ class RegisterController extends Controller
     //
     public function store(Request $request) {
 
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required',
-        ]);
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required',
+                'email' => 'required',
+            ]);
+    
+            //check if validation fails
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 422);
+            }
+    
+            //create user
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+                // 'remember_token' => Str::random(60),
+            ]);
+    
+            return response()->json([
+                'status' => true,
+                'message' => 'Data User Berhasil Ditambahkan!',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ], 200);
 
-        //check if validation fails
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
+            //return response
+            // return new PostResource(true, 'Data User Berhasil Ditambahkan!', $user);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
         }
-
-        //create user
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-            'remember_token' => Str::random(60),
-        ]);
-
-        //return response
-        return new PostResource(true, 'Data User Berhasil Ditambahkan!', $user);
     }
 }
